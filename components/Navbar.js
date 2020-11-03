b264ebfa3b08939db36888ef0171bda532d0d43d
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-11-02 23:34:20 -0600

    feat: create navbar icons

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,15 @@
 import IconButton from '@material-ui/core/IconButton';
 import Input from '@material-ui/core/Input';
 import InputAdornment from '@material-ui/core/InputAdornment';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Icon from '@material-ui/core/Icon';
 
-const Navbar = () => {
-  return (
-    <div className="navbar">
+const Navbar = () => (
+  <div className="navbar">
+    <img src="/images/chaman-morado.png" alt="chaman morado" />
+    <div className="search-bar">
       <Input
-        id="navbar"
+        id="search-bar"
         placeholder="Buscar producto..."
         fullWidth
         endAdornment={
@@ -19,7 +21,25 @@ const Navbar = () => {
         }
       />
     </div>
-  );
-};
+    <div className="icons">
+      <div className="icons__item">
+        <Icon>person</Icon>
+        <p>Mi cuenta</p>
+      </div>
+      <div className="icons__item">
+        <Icon>favorite</Icon>
+        <p>Mis favoritos</p>
+      </div>
+      <div className="icons__item">
+        <Icon>publish</Icon>
+        <p>Cargar pedido masivo</p>
+      </div>
+      <div className="icons__item">
+        <ShoppingCartIcon />
+        <p>Mi carrito</p>
+      </div>
+    </div>
+  </div>
+);
 
 export default Navbar;
